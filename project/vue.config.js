dfa39890dd71728c0740bbff3fc7ec94d9f16259
Firmino Changani <firmino.changani@jungle.ai>
2020-03-25 14:49:13 +0000

    Access scss variables anywhere

---
+++ project/vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `
+          @import "@/assets/styles/partials/_variables.scss";
+        `
+      }
+    }
+  }
+};
