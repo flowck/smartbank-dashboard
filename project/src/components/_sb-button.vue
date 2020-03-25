ec0eaed6924fff68ef41e6cdc78207da90340aff
Firmino Changani <firmino.changani@jungle.ai>
2020-03-25 15:19:26 +0000

    Add button component

---
+++ project/src/components/_sb-button.vue
@@ -0,0 +1,37 @@
+<template>
+  <button :class="classes">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { Prop, Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class SBButton extends Vue {
+  // Props
+  @Prop({ default: false }) isFullWidth: boolean;
+
+  // State properties
+  private classes: any = {
+    "sb-button": true,
+    "sb-button--full-width": this.isFullWidth
+  };
+}
+</script>
+
+<style lang="scss">
+.sb-button {
+  border: none;
+  cursor: pointer;
+  font-weight: bold;
+  padding: 10px 20px;
+  border-radius: 10px;
+  color: $sb-color-white;
+  background-color: $sb-color-blue;
+
+  &.sb-button--full-width {
+    width: 100%;
+  }
+}
+</style>
