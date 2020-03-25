34e1fd93ec24b80be40a9d3f6d0310e39a101219
Firmino Changani <firmino.changani@jungle.ai>
2020-03-25 14:50:10 +0000

    Create card component

---
+++ project/src/components/sb-card.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="sb-card" :style="styles">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { Prop, Component, Vue } from "vue-property-decorator";
+
+// Interface
+interface Style {
+  backgroundColor?: string;
+}
+
+@Component
+export default class SBCard extends Vue {
+  // State properties
+  private styles: Style = {};
+
+  // Props
+  @Prop({ default: "#FFF" }) private backgroundColor!: string;
+
+  private mounted() {
+    // Set styles
+    this.styles.backgroundColor = this.backgroundColor;
+  }
+}
+</script>
+
+<style lang="scss">
+.sb-card {
+  padding: 15px 25px;
+  border-radius: 20px;
+  background-color: #fff;
+  box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.1);
+}
+</style>
