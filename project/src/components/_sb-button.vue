587d5d71688845b882f5458c8e46c6b4e9ba0340
Firmino Changani <firmino.changani@jungle.ai>
2020-03-28 13:57:01 +0000

    Emit on click event

--- project/src/components/_sb-button.vue
+++ project/src/components/_sb-button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button :class="classes">
+  <button @click="onClick" :class="classes">
     <slot></slot>
   </button>
 </template>
@@ -10,25 +10,36 @@ import { Prop, Vue, Component } from "vue-property-decorator";
 @Component
 export default class SBButton extends Vue {
   // Props
-  @Prop({ default: false }) isFullWidth: boolean;
+  @Prop({ default: false }) isFullWidth!: boolean;
 
   // State properties
   private classes: any = {
     "sb-button": true,
     "sb-button--full-width": this.isFullWidth
   };
+
+  // Emit on click event
+  private onClick(event: Event) {
+    return this.$emit("click", event);
+  }
 }
 </script>
 
 <style lang="scss">
 .sb-button {
   border: none;
+  display: block;
   cursor: pointer;
   font-weight: bold;
   padding: 10px 20px;
   border-radius: 10px;
   color: $sb-color-white;
   background-color: $sb-color-blue;
+
+  &:active,
+  &:focus {
+    outline: none;
+  }
 
   &.sb-button--full-width {
     width: 100%;
