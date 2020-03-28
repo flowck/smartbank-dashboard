d21cdba5651ae09cc832268af2d844ed374679de
Firmino Changani <firmino.changani@jungle.ai>
2020-03-28 13:57:19 +0000

    Add search component based on input component

---
+++ project/src/components/_sb-search.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="sb-search">
+    <sb-input
+      v-model="searchParam"
+      :hasLabel="false"
+      :placeholder="placeholder"
+      @enter="$emit('search', searchParam)"
+    >
+      <template v-slot:icon>
+        <i class="fas fa-search"></i>
+      </template>
+    </sb-input>
+  </div>
+</template>
+
+<script lang="ts">
+import { Prop, Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class SBSearch extends Vue {
+  // Props
+  @Prop({ default: "Search" }) placeholder!: string;
+
+  // State property
+  private searchParam: string = "";
+}
+</script>
