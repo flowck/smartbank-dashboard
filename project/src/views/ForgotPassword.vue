0cd27aa532d00728a4772f74740589d879925411
Firmino Changani <firmino.changani@jungle.ai>
2020-03-29 19:55:33 +0100

    Add forgot password view

---
+++ project/src/views/ForgotPassword.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="sb-forgot-password"></div>
+</template>
+
+<script lang="ts">
+import { Vue, Prop, Component } from "vue-property-decorator";
+
+@Component
+export default class SBForgotPassword extends Vue {}
+</script>
+
+<style lang="scss"></style>
