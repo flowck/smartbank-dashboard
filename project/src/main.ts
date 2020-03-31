453243e496eed5157ab4a4b94a2339b4b0cac242
Firmino Changani <firmino.changani@jungle.ai>
2020-03-31 20:27:42 +0100

    Add vee-validator

--- project/src/main.ts
+++ project/src/main.ts
@@ -2,10 +2,22 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
+import { required, email } from "vee-validate/dist/rules";
+import { ValidationProvider, ValidationObserver, extend } from "vee-validate";
+
+// Helper to import components dynamically
 import "./components/globals";
+
 // Main styles
 import "./assets/styles/main.scss";
 import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
+
+// Global componets
+Vue.component("ValidationProvider", ValidationProvider);
+Vue.component("ValidationObserver", ValidationObserver);
+extend("email", email);
+extend("required", required);
 
 Vue.config.productionTip = false;
 
