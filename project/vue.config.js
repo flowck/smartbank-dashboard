b5a8e889015c1dcf870969985d24c3808d480f2e
Firmino Changani <firmino.changani@jungle.ai>
2020-04-03 23:16:49 +0100

    Add a mixins file

--- project/vue.config.js
+++ project/vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
       sass: {
         prependData: `
           @import "@/assets/styles/partials/_variables.scss";
+          @import "@/assets/styles/partials/_mixins.scss";
         `
       }
     }
